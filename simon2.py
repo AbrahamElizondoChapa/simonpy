3298be21cf54dd714f8ad5ae317d87e6152befca
Abraham Elizondo <AbrahamElizondoChapa@users.noreply.github.com>
2024-10-14 14:33:47 -0600

    Update simon2.py
    se agregaron los menus para controlar la dificultad, la opcion que se elija modifica la velocidad en que se reproduce el nombre de cada color, y el tiempo de espera entre color y color. Se quito el boton de quit y se movio al menu superior.

--- simon2.py
+++ simon2.py
@@ -4,64 +4,90 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-global engine, x
+global engine
 engine = None
 
+global usrSeq
 colors = ['red', 'blue', 'yellow', 'green']
-secuence = []
-userSecuence = []
+difficulty = ['easy', 'medium', 'hard', 'very hard', 'epic']
+difficultyData = {
+    "easy": (1000,100),
+    "medium": (500, 150),
+    "hard": (250, 250),
+    "very hard": (150, 300),
+    "epic": (100, 400)
+}
+sequence = []
+usrSeq = []
+userIt = 0
 life = 3
 score = 0
-velocityInit = 100
+velocityInit = 200
+delay = 1000
 x = 0
 incrementalMode = True
 circleFill = 'black'
-outline = 'silver'
+outline = 'white'
+
+def setDifficulty(k):
+    global velocityInit, delay
+    level = k
+    value = difficultyData[k]
+    delay = value[0]
+    speed = value[1]
+    velocityInit = speed
+    print(f"nivel de dificultad: {level} -> {value}")
+    print(f"delay: {delay}, rate: {velocityInit}")
 
 def init_speech_engine():
     global engine
     engine = pyttsx3.init()
-    print(f"get property rate: {engine.getProperty('rate')}")
-    engine.setProperty('rate', 100)  # Inicializa con la velocidad
+    print(f"get property raTe: {engine.getProperty('rate')}")
+    engine.setProperty('rate', 300)  # Inicializa con la velocidad
     print(f"get property rate: {engine.getProperty('rate')}")
     engine.setProperty('language', 'en')
 
 def secGen(sec, color):
     sec.append(color)
 
-def userSecGen(color):
-    userSecuence.append(color)
-    label4.config(text=f"User Secuence: {userSecuence}")
-
 def soundit(txt):
     global engine
     if engine is None:
         init_speech_engine()
-    print(f"get property rate: {engine.getProperty('rate')}")
+    print(f"get property ratee: {engine.getProperty('rate')}")
     engine.say(txt)
     engine.runAndWait()
 
 def showSequence(index=0):
-    if index < len(secuence):
+    global delay
+    if index < len(sequence):
         # Ocultar todos los cuadrantes
         for i in range(4):
             canvas.itemconfig(quadrants[i], fill=circleFill)
-
         # Esperar un breve período antes de mostrar el nuevo color
-        root.after(200, lambda: changeColor(secuence[index], index))
+        root.after(int(delay*0.5), lambda: changeColor(sequence[index], index))
     else:
-        print("Secuencia mostrada: ", secuence)
+        root.after(int(delay*0.3))
+        # Ocultar todos los cuadrantes
+        for i in range(4):
+            canvas.itemconfig(quadrants[i], fill=circleFill)
+        print("Secuencia mostrada: ", sequence)
 
 def changeColor(newColor, index):
+    global delay
     # Iluminar el cuadrante correspondiente
     color_index = colors.index(newColor)
     canvas.itemconfig(quadrants[color_index], fill=newColor)
 
     soundit(newColor)
-    root.after(800, lambda: showSequence(index + 1))  # Esperar 1 segundo para mostrar el siguiente color
+    root.after(delay, lambda: showSequence(index + 1))  # Esperar 1 segundo para mostrar el siguiente color
 
 def updateSquareColor():
-    global x, engine
+    global x, engine, menuDifficulty
+
+    for i in range(len(difficultyData)):
+        menuDifficulty.entryconfig(i,state='disabled')
+
     if engine is None:
         init_speech_engine()
     if incrementalMode:
@@ -72,38 +98,71 @@ def updateSquareColor():
     engine.setProperty('rate', newVel)  # Actualiza la velocidad
     print(f"get property rate: {engine.getProperty('rate')}")
     newColor = random.choice(colors)
-    secGen(secuence, newColor)
+    secGen(sequence, newColor)
     print("Update Simon Sequence")
     showSequence()
-
-def on_button_click(color):
-    userSecGen(color)
 
 def exit():
     soundit("Good Bye!")
     root.destroy()
 
+def click_cuadrante(event, quadrant):
+    global userIt, sequence
+    usrSeq = []
+    print("intro click quadrant:::::::::::::::::::::::::::::::::")
+    print("Secuencia mostrada: ", sequence)
+    print(f"User sequence : {userIt}")
+    secGen(usrSeq,colors[quadrant])
+    print(f"secuencia usr {usrSeq}")
+    print(f"secuencia simon {sequence}")
+
+    for i in range(userIt):
+        print(f"printing i-> {i}")
+
+    i = 0
+    while i <= userIt:
+        print(f"i: {i}")
+        print(f"User sequence : {userIt}")
+        print(f"color secuencia usr {usrSeq[i]}")
+        print(f"color secuencia simon {sequence[i]}")
+        if usrSeq[i] == sequence[i]:
+            print("correcto")
+        else:
+            print("incorrecto")
+        i += 1
+    userIt += 1
+    updateSquareColor()
+
 def main():
-    global label4, canvas, root, quadrants
+    global canvas, root, quadrants, menuDifficulty
     root = Tk()
     frm = ttk.Frame(root, padding=10)
     frm.grid()
-    root.geometry("600x600")
+    root.geometry("450x300")
+    menuBar = tk.Menu(root)
+    menuDifficulty = tk.Menu(menuBar, tearoff=0)
+    
+    for key in difficultyData:
+        print(f"cargando opcion de menu dificultad {key}")
+        menuDifficulty.add_command(label=key, command=lambda key=key: setDifficulty(key))
+
+    menuBar.add_cascade(label='Difficulty',menu=menuDifficulty)
+    menuBar.add_command(label='Exit Game', command=exit)
 
     # Label Simon
-    label1 = tk.Label(frm, text="Simon!", font=("Arial", 24))
+    label1 = tk.Label(frm, text="Simon: ", font=("Arial", 24))
     label1.grid(column=0, row=0, sticky='w')
 
     # Score and Life Labels
-    label2 = tk.Label(frm, text=f"Score: {score}")
-    label2.grid(column=0, row=1, sticky='w')
-    
-    label3 = tk.Label(frm, text=f"Life: {life}")
-    label3.grid(column=0, row=2, sticky='w')
+    label2 = tk.Label(frm, text=f"Life: {life}", fg='red')
+    label2.grid(column=1, row=0, sticky='w')
 
-    # Quit Button
-    button1 = tk.Button(frm, text="Quit", command=exit)
-    button1.grid(column=6, row=0, sticky='e')
+    label3 = tk.Label(frm, text=f"Score: {score}", fg='green')
+    label3.grid(column=2, row=0, sticky='w')
+
+    #start
+    button6 = tk.Button(frm, text="start", command=lambda: updateSquareColor())
+    button6.grid(column=8, row=0, padx=6, pady=6)
 
     # Canvas for drawing
     canvas = Canvas(frm, width=200, height=200)
@@ -113,32 +172,13 @@ def main():
     quadrants = []
     quadrants.append(canvas.create_arc(0, 0, 200, 200, start=0, extent=90, fill=circleFill, outline=outline))   # Rojo
     quadrants.append(canvas.create_arc(0, 0, 200, 200, start=90, extent=90, fill=circleFill, outline=outline))  # Azul
-    quadrants.append(canvas.create_arc(0, 0, 200, 200, start=180, extent=90, fill=circleFill, outline=outline)) # Verde
-    quadrants.append(canvas.create_arc(0, 0, 200, 200, start=270, extent=90, fill=circleFill, outline=outline)) # Amarillo
-
-    # User Sequence Label
-    label4 = tk.Label(frm, text=f"User Secuence: {userSecuence}", width=30)
-    label4.grid(column=1, row=3, columnspan=2)
-
-    # Color buttons
-    button2 = tk.Button(frm, text="red", command=lambda: on_button_click("red"))
-    button2.grid(column=0, row=4, padx=5, pady=5)
+    quadrants.append(canvas.create_arc(0, 0, 200, 200, start=180, extent=90, fill=circleFill, outline=outline)) # Amarillo
+    quadrants.append(canvas.create_arc(0, 0, 200, 200, start=270, extent=90, fill=circleFill, outline=outline)) # Verde
     
-    button3 = tk.Button(frm, text="blue", command=lambda: on_button_click("blue"))
-    button3.grid(column=1, row=4, padx=5, pady=5)
+    for i, quadrant in enumerate(quadrants):
+        canvas.tag_bind(quadrant,"<Button-1>", lambda event, quadrant=i: click_cuadrante(event, quadrant))
     
-    button4 = tk.Button(frm, text="yellow", command=lambda: on_button_click("yellow"))
-    button4.grid(column=2, row=4, padx=5, pady=5)
-    
-    button5 = tk.Button(frm, text="green", command=lambda: on_button_click("green"))
-    button5.grid(column=0, row=5, padx=5, pady=5)
-
-    button6 = tk.Button(frm, text="simon secuence", command=lambda: updateSquareColor())
-    button6.grid(column=4, row=6, padx=6, pady=6)
-    
-    # Inicializa la secuencia
-    updateSquareColor()
-    
+    root.config(menu=menuBar)
     root.mainloop()
 
 main()
